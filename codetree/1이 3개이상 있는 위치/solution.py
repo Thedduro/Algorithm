2a113a69c3ae4a7884d1c16ffe83038505f7289d
unknown <lsw2207@gmail.com>
2025-11-04 10:03:39 +0900

    1이 3개 이상 있는 위치

--- codetree/1이 3개이상 있는 위치/solution.py
+++ codetree/1이 3개이상 있는 위치/solution.py
@@ -1,3 +1,21 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
+def check(i, j):
+    mcnt = 0
+    for di, dj in [(0,1),(0,-1),(1,0),(-1,0)]:
+        ni, nj = i+di, j+dj
+        if 0 <= ni < n and 0<= nj < n:
+            if grid[ni][nj] == 1:
+                mcnt += 1
+    if mcnt >= 3:
+        return True
+    else:
+        return False
+
+cnt = 0
+for i in range(n):
+    for j in range(n):
+        if check(i, j):
+            cnt += 1
+print(cnt)
