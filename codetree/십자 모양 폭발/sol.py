9f2221cc4803760c92900b1b04fad4d0b19c6c3b
unknown <lsw2207@gmail.com>
2025-11-13 17:53:38 +0900

    십자모양 폭발

---
+++ codetree/십자 모양 폭발/sol.py
@@ -0,0 +1,33 @@
+'''
+- 문제:
+    - r,c에 적힌 값만큼 상하좌우의 칸의 값이 0으로 바뀐다.
+    - 터진 칸은 사라지고 위에 있는 칸들의 값이 아래서 부터 다시 차곡차곡 쌓인다.
+- 아이디어:
+    - 터지는 칸들을 리스트에서 삭제하고, 나중에 리스트의 길이를 N을 만족하게끔 0을 추가
+
+'''
+from collections import deque
+
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+r, c = map(int, input().split())
+r, c = r-1, c-1
+
+direction = [(-1,0),(1,0),(0,-1),(0,1)]
+
+visited = set()
+visited.add((r,c))
+
+max_cnt = grid[r][c]
+grid[r][c] = 0
+
+for dx, dy in direction:
+    x, y = r, c
+    for _ in range(max_cnt):
+        nx, ny = x+dx, y+dy
+
+        if 0 <= nx < n and 0 <= ny < n and (nx,ny) not in visited:
+            visited.add((nx,ny))
+            grid[nx][ny] = 0
+            x, y = nx, ny
+        
