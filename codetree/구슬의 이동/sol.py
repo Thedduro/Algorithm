605c6dc22b294f277889be45573fac99c50d9cab
Thedduro <lsw2207@gmail.com>
2025-11-22 15:53:41 +0900

    시뮬_구슬의 이동(반사처리)

--- codetree/구슬의 이동/sol.py
+++ codetree/구슬의 이동/sol.py
@@ -16,7 +16,7 @@ n, m, t, k = map(int, input().split())
 marbles =[]
 for i in range(1, m+1):
     r,c,dir,v = input().split()
-    marbles.append((r,c,dir,v,i))
+    marbles.append((int(r)-1,int(c)-1,dir,int(v),i))
 
 direction = {
     'L': ((0, -1), 'R'),
@@ -26,18 +26,63 @@ direction = {
 }
 
 def moving(x, y, dir, v, i):
-    dx, dy = direction[dir]
-    nx, ny = x + (dx*v), y + (dy*v)
-    if 0 <= nx < n and 0 <= ny < n:
-        return (nx, ny, dir, v, i)
-    else:
-        
-for _ in range(t):
-    move = []
+    dx, dy = direction[dir][0]
+    L = n - 1
+    cycle = 2 * L
 
-    for x, y, dir, v, i in marbles:
-        dx, dy = direction[dir]
-        nx, ny = x + (dx*v), y + (dy*v)
-        if 0 <= nx < n and 0 <= ny < n:
-            
-        next = moving(x, y, dir, v)
+    # x축 이동
+    if dx != 0:
+        raw = x + dx * v
+        moved = raw % cycle
+        nx = moved if moved <= L else cycle - moved
+        
+        # 반사 횟수 = raw // L
+        reflect_count = raw // L
+
+        if reflect_count % 2 == 0:
+            new_dir = dir
+        else:
+            new_dir = direction[dir][1]
+
+        return (nx, y, new_dir, v, i)
+
+    # y축 이동
+    else:
+        raw = y + dy * v
+        moved = raw % cycle
+        ny = moved if moved <= L else cycle - moved
+
+        reflect_count = raw // L
+        
+        if reflect_count % 2 == 0:
+            new_dir = dir
+        else:
+            new_dir = direction[dir][1]
+
+        return (x, ny, new_dir, v, i)
+
+def check(moves):
+    grid = [[[] for _ in range(n)] for _ in range(n)]
+    for x, y, dir, v, i in moves:
+        grid[x][y].append((dir,v,i))
+    
+    result = []
+    for i in range(n):
+        for j in range(n):
+            if grid[i][j]:
+                grid[i][j].sort(key=lambda x: (x[1], x[2]), reverse=True)
+                for dir,v,idx in grid[i][j][:k]:
+                    result.append((i,j,dir,v,idx))
+    return result
+
+
+for _ in range(t):
+    moves = []
+
+    for x, y, dir, v, i in marbles:  
+        next = moving(x, y, dir, v, i)
+        moves.append(next)
+    
+    marbles = check(moves)
+
+print(len(marbles))
