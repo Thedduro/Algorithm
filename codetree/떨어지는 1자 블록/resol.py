70a51f3bb7a7c95288a8167941cde0d600cb0f5d
thedduro <lsw2207@gmail.com>
2025-11-25 16:49:00 +0900

    떨어지는 블록 다시풀기 (시뮬)

---
+++ codetree/떨어지는 1자 블록/resol.py
@@ -0,0 +1,15 @@
+n, m, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(n):
+    row = grid[i]
+    if 1 in row[k-1:k-1+m]:
+        break
+    else:
+        ii = i
+
+for j in range(k-1, k-1+m):
+    grid[ii][j] = 1
+
+for row in grid:
+    print(*row)
